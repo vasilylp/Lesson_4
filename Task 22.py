9352fe585b9b9080d1d709e4d1bba348e0f66f29
Vpolyushkevich@gmail.com <Vasily>
2023-07-07 10:39:56 +0300

    Add new solutions

--- Task 22.py
+++ Task 22.py
@@ -17,23 +17,44 @@ m - кол-во элементов второго множества. Затем
 # list_3 = [n for n in list_1 if n in list_2]
 # print(list_3)
 
-# 2 solution Standart sample ___________________________________________________
-mol = [int(x) for x in input("Введите число :").split()]
-n = mol[0]
-m = mol[1]
-set_1 = set()
-set_2 = set()
-list_1 = list()
-a = [int(x) for x in input("Введите число :").split()]
-k = set(a)
-for i in k:
-    set_1.add(i)
-b = [int(x) for x in input("Введите число :").split()]
-k = set(b)
-for i in k:
-    set_2.add(i)
-lok =set_1 & set_2
-kool = list(lok)
-kool.sort()
-for i in kool:
-    print(i, end = ' ')
+# # 2 solution Standart sample ___________________________________________________
+# mol = [int(x) for x in input("Введите число :").split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input("Введите число :").split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
+# b = [int(x) for x in input("Введите число :").split()]
+# k = set(b)
+# for i in k:
+#     set_2.add(i)
+# lok =set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end = ' ')
+
+# 3 solution____________________________________________________
+# n, m = [int(z) for z in input("Введите размеры списков - две цифры через запятую : ").split(',')]
+# print(n,m)
+# n_list = list(int(num) for num in input(f"Введите {n} элементов 1 списка через пробел : ").strip().split())[:n]
+# m_list = list(int(num) for num in input(f"Введите {m} элементов 2 списка через пробел : ").strip().split())[:m]
+# print("n_list: ", n_list)
+# print("m_list: ", m_list)
+# set_1 = set(n_list)
+# set_2 = set(m_list)
+# print(set_1.intersection(set_2)) # пересечение
+# print(set_1.union(set_2)) # объединение
+
+#____________________
+# a = [1, 2, 3, 3 ,4 ,4, 5, 6]
+# b = [3, 4, 5, 7, 8,  9, 9, 10]
+# set_1 = set()
+# for i in b:
+#     if i in a:
+#         set_1.add(i)
+# print(set_1)
